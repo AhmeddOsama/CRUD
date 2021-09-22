fa870d79515fc06d91ec975cfec27f329358d402
Ahmed Osama <ahmeddosama@hotmail.com>
2021-09-22 02:52:11 +0200

    JWT added

--- src/main/kotlin/services/JWT.kt
+++ src/main/kotlin/services/JWT.kt
@@ -5,26 +5,25 @@ import jakarta.inject.Singleton
 import org.json.JSONObject
 import java.nio.charset.StandardCharsets
 import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
 @Singleton
 class JWT {
+    var secret = System.getenv("SECRET")
+
     fun getJWT(username: String): String {
         var JWT_HEADER = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
-        var secret = "shh"
         var payload = JSONObject()
-        var base: Base64
-        val nowMillis = System.currentTimeMillis()
-        val now = Date(nowMillis)
-        val expMillis: Long = nowMillis
-        val exp = Date(expMillis)
+        val expAt = System.currentTimeMillis()+ TimeUnit.MINUTES.toMillis(30)
+        val issuedAt = System.currentTimeMillis()
 
         payload.put("username", username)
-        payload.put("id", 1)
-        payload.put("exp", exp);
+        payload.put("exp", expAt)
+        payload.put("issuedAt",issuedAt)
 
-        var signature = hmacSha256(
+        var signature = hash(
             base64().encode(JWT_HEADER.toByteArray()) + "." + base64().encode(
                 payload.toString().toByteArray()
             ), secret
@@ -36,11 +35,9 @@ class JWT {
         return token
     }
 
-    private fun hmacSha256(data: String, secret: String): String? {
-
-        //MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    private fun hash(data: String, secret: String): String? {
         val hash: ByteArray =
-            secret.toByteArray(StandardCharsets.UTF_8) //digest.digest(secret.getBytes(StandardCharsets.UTF_8));
+            secret.toByteArray(StandardCharsets.UTF_8)
         val sha256Hmac = Mac.getInstance("HmacSHA256")
         val secretKey = SecretKeySpec(hash, "HmacSHA256")
         sha256Hmac.init(secretKey)
@@ -54,19 +51,21 @@ class JWT {
 
     fun verifyToken(token :String):String
      {
-         var secret = "shh"
          var token = token.split(" ")
          var tokenParts = token[1].split(".")
          var jwtHeader =tokenParts[0]
          var payload =tokenParts[1]
          var signature=tokenParts[2]
-         print(signature+" "+hmacSha256(jwtHeader + "." + payload, secret))
-         if(signature.equals(hmacSha256(jwtHeader+ "." + payload, secret)))
+
+         if(signature.equals(hash(jwtHeader+ "." + payload, secret)))
          {
              payload=String(Base64.getUrlDecoder().decode(tokenParts[1]))
              val jsonPayload = JSONObject(payload)
-             println(jsonPayload.getLong("exp"))
+             if(jsonPayload.getLong("exp") > System.currentTimeMillis())
+             {
+                 return "Verified"+jsonPayload
+             }
          }
-         return payload
+         return "Verificatio Error"
     }
 }
